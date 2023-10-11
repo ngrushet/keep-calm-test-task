f32af33b55f4907a731dd329dc1cc60d5130a6fb
Veronique Doss <vdoss@student.21-school.ru>
2023-10-11 23:19:02 +0700

    third task done

--- third-back-task.ts
+++ third-back-task.ts
@@ -1,42 +1,34 @@
 export default class ThirdBackTask {
     static getResult(s: string): string {
         const stack: (number | string)[] = [];
-        let finalresult: string = "";
-        let stack_string: string = ""
-
-        let temp: string = "";
+        let finalResult: string = "";
         
         for (let i = 0; i < s.length; i++) {
-            console.log(`received char: ${s[i]} `)
             const char = s[i];
-            
+
             if (/\d/.test(char)) {
-                // if (s[i+1] === "{") {
-                    stack.push(Number(char))
-                // } else {
-                //     stack_string += char
-                // }
+                stack.push(Number(char));
+
             } else if (char === "{") {
                 stack.push("{");
+
             } else if (char === "}") {
-                let toRepeat: string = temp
+                let toRepeat: string = "";
 
                 while (stack[stack.length - 1] !== "{") {
                     toRepeat = stack.pop() as string + toRepeat;
-                    console.log(`toRepeat string: ${toRepeat}`)
                 }
-
                 stack.pop(); // Удаляем "{"
-                temp = toRepeat.repeat(stack.pop() as number); 
-                console.log(`temp string: ${temp}`)
+                stack.push(toRepeat.repeat(stack.pop() as number))
             }
         }
-        console.log(`result string before cleaning stack: ${finalresult}`)
 
         while (stack.length !== 0) {
-            finalresult = stack.pop() as string + finalresult;
+            finalResult = stack.pop() as string + finalResult;
         }
 
-        return finalresult;
+        return finalResult;
     }
 }
+
+// console.log(ThirdBackTask.getResult("333333")) 
